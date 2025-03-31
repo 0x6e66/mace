d38ab94d7961d9c39aa18deffd0f764f91aaa1e7
Frondorf, Niklas <niklas@frondorf.eu>
2025-03-31 18:22:54 +0200

    fix(coper): fixed overwriting of tag and customer data in internal stack

--- src/extractor/coper/mod.rs
+++ src/extractor/coper/mod.rs
@@ -3,7 +3,7 @@ mod rules;
 use std::io::{Cursor, Read};
 
 use anyhow::{Result, anyhow};
-use iced_x86::{Code, Decoder, DecoderOptions};
+use iced_x86::{Code, Decoder, DecoderOptions, Mnemonic, Register};
 use yara_x::{Compiler, Scanner};
 use zip::ZipArchive;
 
@@ -66,61 +66,82 @@ fn extract_from_elf(elf_data: &[u8]) -> Result<(Option<String>, Option<String>)>
 
     let mut stack = [0u8; 100];
 
-    'rule: for rule in results.matching_rules() {
-        for pattern in rule.patterns() {
-            for mat in pattern.matches() {
-                let start = mat.range().start;
-                let decoder = Decoder::with_ip(
-                    64,
-                    &elf_data[start..start + 200],
-                    start as u64,
-                    DecoderOptions::NONE,
-                );
+    let mat = results
+        .matching_rules()
+        .next()
+        .and_then(|r| r.patterns().next())
+        .and_then(|p| p.matches().next())
+        .ok_or(anyhow!("could not find 'make_DGA' in binary"))?;
 
-                let mut xmm_data = Vec::new();
+    let start = mat.range().start;
+    let decoder = Decoder::with_ip(
+        64,
+        &elf_data[start..start + 200],
+        start as u64,
+        DecoderOptions::NONE,
+    );
 
-                // for each instruction get data that will be put on the stack
-                for instruction in decoder {
-                    match instruction.code() {
-                        Code::Movaps_xmm_xmmm128 => {
-                            let offset = instruction.memory_displacement64() as usize;
-                            let data = elf_data[offset..offset + 16].to_vec();
+    let mut xmm_data = Vec::new();
+    let mut rax_data = Vec::new();
 
-                            xmm_data = data;
-                        }
-                        Code::Movaps_xmmm128_xmm => {
-                            let base = instruction.memory_displacement64() as usize;
+    // for each instruction get data that will be put on the stack
+    for instruction in decoder.into_iter().skip(1) {
+        if matches!(instruction.mnemonic(), Mnemonic::Call) {
+            break;
+        }
+        match instruction.code() {
+            Code::Movaps_xmm_xmmm128 => {
+                let offset = instruction.memory_displacement64() as usize;
+                let data = elf_data[offset..offset + 16].to_vec();
 
-                            for (i, e) in xmm_data.iter().enumerate() {
-                                stack[base + i] = *e;
-                            }
-                        }
-                        Code::Mov_rm32_imm32 => {
-                            let base = instruction.memory_displacement64() as usize;
+                xmm_data = data;
+            }
+            Code::Movaps_xmmm128_xmm => {
+                let base = instruction.memory_displacement64() as usize;
 
-                            for (i, e) in instruction.immediate32().to_le_bytes().iter().enumerate()
-                            {
-                                stack[base + i] = *e;
-                            }
-                        }
-                        Code::Mov_rm16_imm16 => {
-                            let base = instruction.memory_displacement64() as usize;
-
-                            for (i, e) in instruction.immediate16().to_le_bytes().iter().enumerate()
-                            {
-                                stack[base + i] = *e;
-                            }
-                        }
-                        Code::Mov_rm8_imm8 => {
-                            let base = instruction.memory_displacement64() as usize;
-
-                            stack[base] = instruction.immediate8();
-                        }
-                        _ => (),
-                    }
+                for (i, e) in xmm_data.iter().enumerate() {
+                    stack[base + i] = *e;
                 }
             }
-            break 'rule;
+            Code::Mov_r64_imm64 => {
+                let displ_data = instruction.memory_displacement32().to_le_bytes();
+                let immediate_data = instruction.immediate32().to_le_bytes();
+
+                rax_data = [immediate_data, displ_data].concat();
+            }
+            Code::Mov_rm64_r64 => {
+                if matches!(instruction.memory_base(), Register::None)
+                    || !(matches!(instruction.op0_register(), Register::None)
+                        && matches!(instruction.op1_register(), Register::RAX))
+                {
+                    continue;
+                }
+                let base = instruction.memory_displacement64() as usize;
+
+                for (i, e) in rax_data.iter().enumerate() {
+                    stack[base + i] = *e;
+                }
+            }
+            Code::Mov_rm32_imm32 => {
+                let base = instruction.memory_displacement64() as usize;
+
+                for (i, e) in instruction.immediate32().to_le_bytes().iter().enumerate() {
+                    stack[base + i] = *e;
+                }
+            }
+            Code::Mov_rm16_imm16 => {
+                let base = instruction.memory_displacement64() as usize;
+
+                for (i, e) in instruction.immediate16().to_le_bytes().iter().enumerate() {
+                    stack[base + i] = *e;
+                }
+            }
+            Code::Mov_rm8_imm8 => {
+                let base = instruction.memory_displacement64() as usize;
+
+                stack[base] = instruction.immediate8();
+            }
+            _ => (),
         }
     }
 
@@ -130,7 +151,7 @@ fn extract_from_elf(elf_data: &[u8]) -> Result<(Option<String>, Option<String>)>
         .filter_map(|bytes| String::from_utf8(bytes.to_vec()).ok())
         .collect();
 
-    let tag = tmp.get(0).cloned();
+    let tag = tmp.first().cloned();
     let customer = tmp.get(1).cloned();
 
     Ok((customer, tag))
