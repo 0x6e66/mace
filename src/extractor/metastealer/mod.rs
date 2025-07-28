27d55dd59c88061638afcee7dab5f07a0510b6bc
Frondorf, Niklas <niklas@frondorf.eu>
2025-07-28 20:00:07 +0200

    chore(metastealer): changed rule to detect seed parameter

--- src/extractor/metastealer/mod.rs
+++ src/extractor/metastealer/mod.rs
@@ -2,21 +2,16 @@ mod rules;
 
 use anyhow::{Result, anyhow};
 use exe::{Buffer, PEType, VecPE};
-use iced_x86::{Code, Decoder, DecoderOptions, Mnemonic};
+use iced_x86::{Code, Decoder, DecoderOptions, Instruction};
 use yara_x::{Compiler, Scanner};
 
-use crate::{
-    configuration::MalwareConfiguration,
-    extractor::metastealer::rules::{RULE_PARAMS, RULE_SEED},
-    utils::get_bitness_from_pe,
-};
+use crate::{configuration::MalwareConfiguration, extractor::metastealer::rules::RULE_SEED};
 
 pub fn extract(sample_data: &[u8]) -> Result<MalwareConfiguration> {
     let pe = VecPE::from_data(PEType::Disk, sample_data);
 
     // extract seeds and other dga parameters
     let seed = get_seed(&pe).ok_or(anyhow!("seed not found"))?;
-    let params = get_dga_params(&pe).ok_or(anyhow!("dga parameters not found"))?;
 
     // get config from sample data
     let mut res = MalwareConfiguration::from((sample_data, "Metastealer"));
@@ -24,94 +19,12 @@ pub fn extract(sample_data: &[u8]) -> Result<MalwareConfiguration> {
 
     // add seed and parameters to config
     mn.insert("seed".to_string(), seed);
-    mn.insert("num_of_domains".to_string(), params.num.into());
-    mn.insert("mul_value".to_string(), params.mul.into());
-    mn.insert("len_of_domain".to_string(), params.len.into());
-    mn.insert("and_value".to_string(), params.and.into());
-    mn.insert("div_value".to_string(), params.div.into());
-    mn.insert("add_value".to_string(), params.add.into());
-    mn.insert("xor_value".to_string(), params.xor.into());
 
     Ok(res)
 }
 
-#[derive(Debug)]
-struct DgaParams {
-    pub num: u32,
-    pub mul: u32,
-    pub len: u32,
-    pub and: u32,
-    pub div: u32,
-    pub add: u8,
-    pub xor: u8,
-}
-
-fn get_dga_params(pe: &VecPE) -> Option<DgaParams> {
-    let bitness = get_bitness_from_pe(pe);
-
-    // compile yara rule and get results from scanner
-    let mut compiler = Compiler::new();
-    compiler.add_source(RULE_PARAMS).ok()?;
-    let rules = compiler.build();
-    let mut scanner = Scanner::new(&rules);
-    let results = scanner.scan(pe.get_buffer().as_slice()).ok()?;
-
-    let mut num: Option<u32> = None;
-    let mut mul: Option<u32> = None;
-    let mut len: Option<u32> = None;
-    let mut and: Option<u32> = None;
-    let mut div: Option<u32> = None;
-    let mut add: Option<u8> = None;
-    let mut xor: Option<u8> = None;
-
-    for r in results.matching_rules() {
-        for pat in r.patterns() {
-            for mat in pat.matches() {
-                let decoder = Decoder::new(bitness, mat.data(), DecoderOptions::NONE);
-
-                for instruction in decoder {
-                    match instruction.code() {
-                        Code::Cmp_rm32_imm32 if num.is_none() => {
-                            num = Some(instruction.immediate32());
-                        }
-                        Code::Imul_r32_rm32_imm32 if mul.is_none() => {
-                            mul = Some(instruction.immediate32());
-                        }
-                        Code::Mov_rm32_imm32 if len.is_none() && instruction.len() == 10 => {
-                            len = Some(instruction.immediate32());
-                        }
-                        Code::And_EAX_imm32 if and.is_none() => {
-                            and = Some(instruction.immediate32());
-                        }
-                        Code::Mov_r32_imm32 if div.is_none() && instruction.immediate32() != 0 => {
-                            div = Some(instruction.immediate32());
-                        }
-                        Code::Add_rm8_imm8 if add.is_none() => {
-                            add = Some(instruction.immediate8());
-                        }
-                        Code::Xor_rm32_imm8 if xor.is_none() => {
-                            xor = Some(instruction.immediate8());
-                        }
-                        _ => (),
-                    }
-                }
-            }
-        }
-    }
-
-    Some(DgaParams {
-        num: num?,
-        mul: mul?,
-        len: len?,
-        and: and?,
-        div: div?,
-        add: add?,
-        xor: xor?,
-    })
-}
-
 fn get_seed(pe: &VecPE) -> Option<u64> {
-    let bitness = get_bitness_from_pe(pe);
+    let bitness = 32;
 
     // compile yara rule and get results from scanner
     let mut compiler = Compiler::new();
@@ -131,8 +44,9 @@ fn get_seed(pe: &VecPE) -> Option<u64> {
         let decoder = Decoder::new(bitness, mat.data(), DecoderOptions::NONE);
         if let Some(instruction) = decoder
             .into_iter()
-            .filter(|i| matches!(i.mnemonic(), Mnemonic::Push))
-            .find(|i| ![0x6ef, 0].contains(&i.immediate32()))
+            .filter(|i| matches!(i.code(), Code::Pushd_imm32))
+            .collect::<Vec<Instruction>>()
+            .last()
         {
             return Some(instruction.immediate64());
         }
--- src/extractor/metastealer/rules.rs
+++ src/extractor/metastealer/rules.rs
@@ -5,80 +5,57 @@ rule seed {
 
     strings:
         $seed = {
-            89 65 f0              // mov        dword ptr [ebp + local], esp
-            8b c1                 // mov        eax, ecx                         
-            89 85 8c fd ff ff     // mov        dword ptr [ebp + local], eax
-            8b 7b 08              // mov        edi, dword ptr [ebx + param_1]
-            68 ef 06 00 00        // push       0x6ef
-            89 85 84 fd ff ff     // mov        dword ptr [ebp + localc], eax
-            8d 85 f0 fd ff ff     // lea        eax => local, [ebp + 0xfffffdf0]
-            68 ?? ?? ?? ??        // push       seed
-            50                    // push       eax
-            89 bd 88 fd ff ff     // mov        dword ptr [ebp + local], edi
-            c7 45 fc 00 00 00 00  // mov        dword ptr [ebp + local], 0x0
-            e8 ?? ?? ?? ??        // call       dga
-        }
+            (
+                68 ?? ?? ?? ??        // push       value
+                89 85 ?? ?? ?? ??     // mov        dword ptr [ebp + localc], eax
+                8d 85 ?? ?? ?? ??     // lea        eax => local, [ebp + offset]
+                68 ?? ?? ?? ??        // push       seed
+                |
+                8d 85 ?? ?? ?? ??     // lea        eax => local, [ebp + offset]
+                68 ?? ?? ?? ??        // push       value
+                68 ?? ?? ?? ??        // push       seed
+                |
+                8d 85 ?? ?? ?? ??     // lea        eax => local, [ebp + offset]
+                89 85 ?? ?? ?? ??     // mov        dword ptr [ebp + localc], eax
+                68 ?? ?? ?? ??        // push       value
+                68 ?? ?? ?? ??        // push       seed
+                |
+                8d 96 ?? ?? ?? ??     // lea        edx => local, [esi + value]
+                c7 86 [8]             // mov        dword ptr [esi + local], value
+                68 ?? ?? ?? ??        // push       value
+                68 ?? ?? ?? ??        // push       seed
+                |
+                c7 86 [8]             // mov        dword ptr [esi + local], value
+                68 ?? ?? ?? ??        // push       value
+                68 ?? ?? ?? ??        // push       seed
+                |
+                c7 86 [8]             // mov        dword ptr [esi + local], value
+                68 ?? ?? ?? ??        // push       value
+                68 ?? ?? ?? ??        // push       seed
+                8d 86 ?? ?? ?? ??     // lea        eax => local, [esi + value]
+            )
 
-    condition:
-        all of them
-}"#;
+            (
+                50                    // push       eax
+                |
+                52                    // push       edx
+            )
 
-pub static RULE_PARAMS: &str = r#"
-rule params {
-    meta: 
-        author = "Frondorf, Niklas"
-    strings:
-        $params = {
-            0f 57 c0     // XORPS      XMM0,XMM0
-            81 fe ??     // CMP        ESI,0x2710
-            ?? ?? ??
-            0f 83 ??     // JNC        LAB_100da487
-            ?? ?? ??
-            8d 4e 01     // LEA        ECX,[ESI + 0x1]
-            c7 45 98     // MOV        dword ptr [EBP + local_88],0x0
-            00 00 00 00
-            0f af 8d     // IMUL       ECX,dword ptr [EBP + local_1d4]
-            4c fe ff ff
-            0f af f2     // IMUL       ESI,EDX
-            0f 11 45 88  // MOVUPS     xmmword ptr [EBP + local_98[0]],XMM0
-            c7 45 9c     // MOV        dword ptr [EBP + local_88+0x4],0xf
-            0f 00 00 00
-            c6 45 88 00  // MOV        byte ptr [EBP + local_98[0]],0x0
-            03 c9        // ADD        ECX,ECX
-            69 f6 ??     // IMUL       ESI,ESI,0xf6
-            ?? ?? ??
-            89 8d 64     // MOV        dword ptr [EBP + local_1bc],ECX
-            fe ff ff
-            33 f1        // XOR        ESI,ECX
-            c7 45 fc     // MOV        dword ptr [EBP + local_24],0x0
-            00 00 00 00
-            33 ff        // XOR        EDI,EDI
-            c7 85 68     // MOV        dword ptr [EBP + local_1b8],0x10
-            fe ff ff 
-            ?? ?? ?? ??
-                         //     LAB_100d99a6
-            8b c6        // MOV        EAX,ESI
-            33 d2        // XOR        EDX,EDX
-            25 ?? ??     // AND        EAX,0x7fffffff
-            ?? ??
-            b9 ?? ??     // MOV        ECX,0x1a
-            ?? ??
-            f7 f1        // DIV        ECX
-            8d 4d 88     // LEA        ECX=>local_98,[EBP + -0x78]
-            80 c2 ??     // ADD        DL,0x61
-            0f b6 c2     // MOVZX      EAX,DL
-            50           // PUSH       EAX
-            e8 ?? ??     // CALL       FUN_10119d80
-            ?? ??
-            8b c7        // MOV        EAX,EDI
-            03 bd 64     // ADD        EDI,dword ptr [EBP + local_1bc]
-            fe ff ff
-            33 c6        // XOR        EAX,ESI
-            83 f0 ??     // XOR        EAX,0x34
-            03 f0        // ADD        ESI,EAX
-            83 ad 68     // SUB        dword ptr [EBP + local_1b8],0x1
-            fe ff ff 01
-            75 c9        // JNZ        LAB_100d99a6
+            (
+                89 bd ?? ?? ?? ??     // mov        dword ptr [ebp + local], edi
+                c7 45 fc 00 00 00 00  // mov        dword ptr [ebp + local], 0x0
+                e8 ?? ?? ?? ??        // call       dga
+                |
+                89 b5 ?? ?? ?? ??     // mov        dword ptr [ebp + local], esi
+                89 bd ?? ?? ?? ??     // mov        dword ptr [ebp + local], edi
+                c7 45 fc 00 00 00 00  // mov        dword ptr [ebp + local], 0x0
+                e8 ?? ?? ?? ??        // call       dga
+                |
+                c5 f8 77              // vzeroupper
+                e8 ?? ?? ?? ??        // call       dga
+                |
+                e8 ?? ?? ?? ??        // call       dga
+            )
         }
 
     condition:
